2fa2ec18259c5e3df12283c8d12cdc85483fd79a
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-12-01 12:56:59 -0800

    updates about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,9 +5,16 @@ var About = (props) => {
   return (
     <div>
         <h1 className="text-center">About Component</h1>
-
         <p>This is an applications that allows users to display find the temperature in different cities. It is built using Javascript & React using JSX. It also consumes the OpenWeatherMap API to gather temperature.</p>
+
+      <p>Here are a few places where you can learn about the project and me:</p>
         <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This is the Javascript Framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+          </li>
           <li>
             <a href="https://github.com/kevin-kabore/ReactWeather">Github Repository</a>
           </li>
@@ -16,7 +23,6 @@ var About = (props) => {
           </li>
         </ul>
     </div>
-
   )
 }
 
